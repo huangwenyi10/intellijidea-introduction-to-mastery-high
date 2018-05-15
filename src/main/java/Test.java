af447811b2444dade90500333cb9d12392b0f1d7
Ay <wenyi.huang@e-vada.com>
2018-05-15 17:40:09 +0800

    md

---
+++ src/main/java/Test.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Ay
+ * @create 2018/05/12
+ **/
+public class Test {
+
+    public static final String s = "";
+    public static final String  str = "";
+    
+    //private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+
+
+        String temp = "{\"id\":\"1\",\"name\":\"ay\"}";
+
+        if (args != null) {
+
+        }
+
+        if (args != null) {
+
+        }
+
+        if (args == null) {
+
+        }
+        System.out.println(args);
+
+
+        System.out.println();
+        System.out.println("str = " + str);
+        System.out.printf("");
+        System.out.println("Test.main");
+
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        List userList = new ArrayList();
+        for (int i = 0; i < userList.size(); i++) {
+
+
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+        }
+        String[] userArray = new String[10];
+        for (int i = 0; i < userArray.length; i++) {
+            String s = userArray[i];
+        }
+
+        System.out.println("-----------------");
+
+
+    }
+
+
+    public void test(){
+
+
+        try {
+            test2();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void test2(){
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
+
+
+
+echo "# intellijidea-introduction-to-mastery-high" >> README.md
+idea init
+idea add README.md
+idea commit -m "first commit"
+idea remote add origin git@github.com:huangwenyi10/intellijidea-introduction-to-mastery-high.git
+idea push -u origin master
+
